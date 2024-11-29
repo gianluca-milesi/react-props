19d4951677b803d078644acb0394596646a1dd72
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-29 09:38:31 +0100

    ADD: different color for each tag

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -4,20 +4,29 @@ import placeHolderImage from "../../assets/placeholder.png"
 
 function PostCard({ image, title, tags, content }) {
 
-    const tagsToString = tags.join(", ")
-    // function tagsColor(tag) {
-    //     if (tag === "html") return "red";
-    //     if (tag === "css") return "blue";
-    //     if (tag === "js") return "yellow";
-    //     return "black";
-    // }
+    // const tagsToString = tags.join(", ")
+    function tagsColor(tag) {
+        if (tag === "html") return "red";
+        if (tag === "css") return "blue";
+        if (tag === "js") return "yellow";
+        return "black";
+    }
 
     return (
         <div className={style.card}>
             <img className={style.thumbnail} src={image || placeHolderImage} />
             <div className={style.card_body}>
                 <h3>{title}</h3>
-                <p>{tagsToString}</p>
+                <div>
+                    {tags.map((tag, index) => (
+                        <span
+                            key={index}
+                            style={{ color: tagsColor(tag) }}
+                        >
+                            {tag}
+                        </span>
+                    ))}
+                </div>
                 <p>{content}</p>
                 <Button />
             </div>
--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -6,19 +6,16 @@ function Tags({ tags }) {
     let allTags = [];
     let uniqueTags = [];
 
-    // Estrai tutti i tag dai post
     posts.forEach(post => {
-        allTags = allTags.concat(post.tags); // Unisci tutti i tag in un unico array
+        allTags = allTags.concat(post.tags);
     });
 
-    // Rimuovi i duplicati con un ciclo
     for (let i = 0; i < allTags.length; i++) {
         if (!uniqueTags.includes(allTags[i])) {
-            uniqueTags.push(allTags[i]); // Aggiungi solo i tag che non sono già presenti
+            uniqueTags.push(allTags[i]);
         }
     }
 
-    // Unisci i tag unici in una stringa separata da virgola
     const tagsToString = uniqueTags.join(", ");
 
     return (
